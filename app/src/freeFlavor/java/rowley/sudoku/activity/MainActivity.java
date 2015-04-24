3768a35220991c4fe6b7a2508cb63201165451dc
Joe Rowley <jrowley2@gmail.com>
2015-04-23 22:21:43 -0600

    Setting up crashlytics for release and ad for free flavor

--- app/src/freeFlavor/java/rowley/sudoku/activity/MainActivity.java
+++ app/src/freeFlavor/java/rowley/sudoku/activity/MainActivity.java
@@ -1,7 +1,23 @@
 package rowley.sudoku.activity;
 
+import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import rowley.sudoku.R;
+
 /**
  * Created by joe on 4/23/15.
  */
 public class MainActivity extends MainActivityBuildVariant {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
 }
--- app/src/release/java/rowley/sudoku/activity/MainActivityBuildVariant.java
+++ app/src/release/java/rowley/sudoku/activity/MainActivityBuildVariant.java
@@ -1,8 +1,18 @@
 package rowley.sudoku.activity;
 
+import android.os.Bundle;
+
+import com.crashlytics.android.Crashlytics;
+
 /**
  * Created by joe on 4/23/15.
  */
 public class MainActivityBuildVariant extends MainActivityParent {
-    //todo start crashlytics
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Crashlytics.start(this);
+    }
 }
